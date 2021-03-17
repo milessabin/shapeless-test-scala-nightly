ac1b2342408af3e96e7ba9abc1b5dec818f1a126
Miles Sabin <miles@milessabin.com>
2021-03-17 16:20:50 +0000

    Switch to github actions

--- run.sh
+++ run.sh
@@ -1,12 +1,27 @@
-BRANCH="$1"
+SCALA_BRANCH="$1"
+SHAPELESS_BRANCH="$2"
 
-SCALA_VERSION=$(wget -q -O - https://raw.githubusercontent.com/scala/community-builds/$BRANCH/nightly.properties | grep '^nightly=' | cut -d '=' -f2)
+SCALA_VERSION=$(wget -q -O - https://raw.githubusercontent.com/scala/community-builds/$SCALA_BRANCH/nightly.properties | grep '^nightly=' | cut -d '=' -f2)
+SUFFIX=$(echo $SCALA_BRANCH | tr '.' '_' | cut -d '_' -f1-2)
 
-echo "Branch: $BRANCH"
+echo "Scala branch: $SCALA_BRANCH"
 echo "Scala version: $SCALA_VERSION"
+echo "Project suffix: $SUFFIX"
+echo "shapeless branch: $SHAPELESS_BRANCH"
+
+case $SHAPELESS_BRANCH in
+  master)
+    TESTS="validateJVM"
+    ;;
+  *)
+    echo "Unknown shapeless branch $SHAPELESS_BRANCH"
+    exit 1
+esac
+
+echo "Tests:" $TESTS
 
 rm -rf shapeless/ && \
 git clone https://github.com/milessabin/shapeless.git && \
 cd shapeless && \
-git checkout master && \
-sbt 'set resolvers in Global += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/"' ++$SCALA_VERSION validateJVM
+git checkout $SHAPELESS_BRANCH && \
+sbt 'set resolvers in Global += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/"' ++$SCALA_VERSION $TESTS
